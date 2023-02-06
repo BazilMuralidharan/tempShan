416961bd227ae4d141b67fde8cb6dd9f9d6fc3db
BazilMuralidharan <92780543+BazilMuralidharan@users.noreply.github.com>
2023-02-06 16:07:57 +0530

    in

---
+++ data.js
@@ -0,0 +1,126 @@
+export const fuelOptions = [
+    { value: 'Gasoline', label: 'Gasoline' },
+    { value: 'Diesel', label: 'Diesel' },
+    { value: 'Hybrid', label: 'Hybrid' },
+    { value: 'Electric', label: 'Electric' }
+];
+
+export const bodyConditionOptions = [
+    { value: 'Perfect inside and out', label: 'Perfect inside and out' },
+    { value: 'No accidents, very few faults', label: 'No accidents, very few faults' },
+    { value: 'A bit of wear & tear, all repaired', label: 'A bit of wear & tear, all repaired' },
+    { value: 'Normal wear & tear, a few issues', label: 'Normal wear & tear, a few issues' },
+    { value: 'Lots of wear & tear to the body', label: 'Lots of wear & tear to the body' }
+
+];
+
+export const mecConditionOptions = [
+    { value: 'Perfect inside and out', label: 'Perfect inside and out' },
+    { value: 'Minor faults, all fixed', label: 'Minor faults, all fixed' },
+    { value: 'Major faults, all fixed', label: 'Major faults, all fixed' },
+    { value: 'Major faults fixed, small remain', label: 'Major faults fixed, small remain' },
+    { value: 'Ongoing minor & major faults', label: 'Ongoing minor & major faults' }
+];
+
+export const colorOptions = [
+    { value: 'Red', label: 'Red' },
+    { value: 'Green', label: 'Green' },
+    { value: 'White', label: 'White' },
+    { value: 'Blue', label: 'Blue' },
+    { value: 'Black', label: 'Black' },
+    { value: 'Orange', label: 'Orange' },
+    { value: 'Yellow', label: 'Yellow' },
+    { value: 'Silver', label: 'Silver' },
+    { value: 'Purple', label: 'Purple' },
+    { value: 'Maroon', label: 'Maroon' },
+    { value: 'Brown', label: 'Brown' },
+    { value: 'Neon', label: 'Neon' },
+    { value: 'Turquoise', label: 'Turquoise' },
+    { value: 'Violet', label: 'Violet' },
+];
+
+export const doorsOptions = [
+    { value: 2, label: '2 Door' },
+    { value: 3, label: '3 Door' },
+    { value: 4, label: '4 Door' },
+    { value: '5+ Doors', label: '5+ Doors' }
+];
+
+export const cylinderCountOptions = [
+    { value: 3, label: '3' },
+    { value: 4, label: '4' },
+    { value: 5, label: '5' },
+    { value: 6, label: '6' },
+    { value: 7, label: '7' },
+    { value: 8, label: '8' },
+    { value: 9, label: '9' },
+    { value: 10, label: '10' },
+    { value: 11, label: '11' },
+    { value: 12, label: '12' },
+    { value: 'Unknown', label: 'Unknown' },
+    { value: 'None', label: 'None' }
+
+];
+
+export const bodyTypeOptions = [
+    { value: 'Micro', label: 'Micro' },
+    { value: 'Hatchback', label: 'Hatchback' },
+    { value: 'Sedan', label: 'Sedan' },
+    { value: 'SUV', label: 'SUV' },
+    { value: 'Coupe', label: 'Coupe' },
+    { value: 'Muscle', label: 'Muscle' },
+    { value: 'Pickup', label: 'Pickup' },
+    { value: 'Limousine', label: 'Limousine' }
+];
+
+export const horsePowerOptions = [
+    { value: 'Less than 150 HP', label: 'Less than 150 HP' },
+    { value: '150 - 200 HP', label: '150 - 200 HP' },
+    { value: '200 - 300 HP', label: '200 - 300 HP' },
+    { value: '300 - 400 HP', label: '300 - 400 HP' },
+    { value: '400 - 500 HP', label: '400 - 500 HP' },
+    { value: '500 - 600 HP', label: '500 - 600 HP' },
+    { value: '600 - 700 HP', label: '600 - 700 HP' },
+    { value: '700 - 800 HP', label: '700 - 800 HP' },
+    { value: '800 - 900 HP', label: '800 - 900 HP' },
+    { value: '900+ HP', label: '900+ HP' },
+    { value: 'Unknown', label: 'Unknown' }
+];
+
+export const streeingSideOptions = [
+    { value: 'Left side', label: 'Left side' },
+    { value: 'Right side', label: 'Right side' }
+];
+
+export const ownerOptions = [
+    { value: '1st Owner', label: '1st Owner' },
+    { value: '2nd Owner', label: '2nd Owner' },
+    { value: 'more than 2', label: 'more than 2' }
+];
+
+export const transmissionOptions = [
+    { value: 'Manual', label: 'Manual' },
+    { value: 'Automatic', label: 'Automatic' },
+    { value: 'Semi automatic', label: 'Semi automatic' }
+];
+//bazilCode_startsHere
+export const featureExtraCheckBoxOption = [    
+    {label:  "Leather Seats", value: false, item:"leatherSeats"},
+    {label:  "Parking Sensors", value: false, item:"parkingSensors"},
+    {label:  "Navigation", value: false, item:"navigation"},
+    {label:  "Front wheel drive", value: false, item:"frontWheelDrive"},
+    {label:  "Rear View Cameras", value: false, item:"rearViewCameras"},
+    {label:  "4 Wheel Drive", value: false, item:"fourWheelDrive"},
+    {label:  "Keyless Entry", value: false, item:"keylessEntry"},
+    {label:  "Bluetooth System", value: false, item:"bluetoothSystem"},
+    {label:  "Fog Lights", value: false, item:"fogLight"},
+    {label:  "Anti-Lock Brakes/ABS", value: false, item:"antiLockBrake"},
+    {label:  "Heated Seats", value: false, item:"heatedSeat"},
+    {label:  "Sunroof", value: false, item:"sunRoof"},
+    {label:  "Moonroof", value: false, item:"moonRoof"} ,
+    {label: "All Wheel Drive", value: false , item:"allWheelDrive"},
+    {label: "Anti theft system", value: false , item:"antiTheftSystem"},
+    {label:"Cruise Control", value:false ,item:"cruiseControl"},
+    {label:"Keyless Start", value:false ,item:"keylessStart"}
+]
+//bazilCode_endHere
